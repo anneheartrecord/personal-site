580e32b86adc5b04a9d3bc5f165e5a654d9322d4
anneheartrecord <chengxisheng777@gmail.com>
2026-04-04 10:06:25 +0800

    Add Friends page with situ2001

---
+++ src/data/friends.ts
@@ -0,0 +1,15 @@
+export interface Friend {
+  name: string;
+  url: string;
+  avatar: string;
+  description: string;
+}
+
+export const friends: Friend[] = [
+  {
+    name: "situ2001",
+    url: "https://situ2001.com/",
+    avatar: "https://situ2001.com/avatar.png",
+    description: "Explore with curiosity. Build with empathy.",
+  },
+];
