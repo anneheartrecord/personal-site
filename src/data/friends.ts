813444f87fce2dd405e5f8c2b0e1627a396f81a6
Anneheartrecord <chengxisheng777@gmail.com>
2026-05-28 12:00:55 +0800

    chore: add kawhi and zbwer to friends list

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -12,4 +12,16 @@ export const friends: Friend[] = [
     avatar: "https://situ2001.com/avatar.png",
     description: "Explore with curiosity. Build with empathy.",
   },
+  {
+    name: "kawhi",
+    url: "https://kawhicurry.github.io/",
+    avatar: "https://gallery-cos.kawhicurry.online/profile/avatar.png",
+    description: "SRE, building a reliable world with infinite possibility.",
+  },
+  {
+    name: "zbwer",
+    url: "https://blog.zbwer.work/",
+    avatar: "https://github.com/zbwer.png",
+    description: "UESTC / Cat Cult / Front-End Developer.",
+  },
 ];
